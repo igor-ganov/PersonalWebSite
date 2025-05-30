2a6a9e4cfeaccb42d692ece6b4ebbc990e7c4383
PForever <igor_ganov@live.ru>
2025-05-30 20:02:38 +0200

    fix: image for mobile layout

--- src/_includes/layout/Layout.tsx
+++ src/_includes/layout/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children, title, className }) => {
       <html lang="en">
         <head>
           <script src="./assets/scripts/polyfill.js"></script>
-          <link rel="stylesheet" href="./assets/styles.css" />
+          <link rel="stylesheet" href="./assets/styles.css"/>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>{title}</title>
